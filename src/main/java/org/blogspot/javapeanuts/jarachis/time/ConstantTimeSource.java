63d3b9db3b31f71ebe68d7459c4f4d4c609dfe54
Pietro Martinelli <amicofragile@gmail.com>
2012-12-03 16:31:33 +0000

    Fixed data-tampering potential issue in ConstantTimeSource.currentTime

--- src/main/java/org/blogspot/javapeanuts/jarachis/time/ConstantTimeSource.java
+++ src/main/java/org/blogspot/javapeanuts/jarachis/time/ConstantTimeSource.java
@@ -4,15 +4,12 @@ import java.util.Date;
 
 public class ConstantTimeSource implements TimeSource {
 	private final long timeMillis;
-	private final Date time;
 	
 	public ConstantTimeSource(long timeMillis) {
 		this.timeMillis = timeMillis;
-		this.time = new Date(timeMillis);
 	}
 
 	public ConstantTimeSource(Date aDate) {
-		this.time = aDate;
 		this.timeMillis = aDate.getTime();
 	}
 
@@ -23,6 +20,6 @@ public class ConstantTimeSource implements TimeSource {
 
 	@Override
 	public Date currentTime() {
-		return time;
+		return new Date(timeMillis);
 	}
 }
