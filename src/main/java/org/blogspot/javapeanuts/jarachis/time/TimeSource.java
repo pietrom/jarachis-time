591a97dac6e3607f306e15b93f2ba789fb659f09
Pietro Martinelli <amicofragile@gmail.com>
2012-12-03 21:24:56 +0100

    Site checkin for project jarachis-time

--- src/main/java/org/blogspot/javapeanuts/jarachis/time/TimeSource.java
+++
@@ -1,9 +0,0 @@
-package org.blogspot.javapeanuts.jarachis.time;
-
-import java.util.Date;
-
-public interface TimeSource {
-	public abstract long currentTimeMillis();
-
-	public abstract Date currentTime();
-}
