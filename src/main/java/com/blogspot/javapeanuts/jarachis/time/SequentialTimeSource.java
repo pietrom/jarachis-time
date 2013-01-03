76e8c04b11832acc5689c0b83778216fa901d097
Pietro Martinelli <amicofragile@gmail.com>
2013-01-03 15:59:19 +0000

    DOC Added Javadoc comments

--- src/main/java/com/blogspot/javapeanuts/jarachis/time/SequentialTimeSource.java
+++ src/main/java/com/blogspot/javapeanuts/jarachis/time/SequentialTimeSource.java
@@ -1,19 +1,38 @@
 package com.blogspot.javapeanuts.jarachis.time;
 
 import java.util.Date;
-
+/**
+ * {@link TimeSource} implementation which returns an element of initially provided sequence of values at every <tt>currentTime[Millis]</tt> invocation.
+ * @author Pietro Martinelli
+ *
+ */
 public class SequentialTimeSource implements TimeSource {
+	/**
+	 * Fixed current time sequence provided from client and returned by
+	 * {@link SequentialTimeSource} instance.
+	 */
 	private final long[] timeMillis;
+
+	/**
+	 * Index of time in time sequence returned by next invokation of
+	 * <tt>currentTime[Millis]</tt>.
+	 */
 	private int current = 0;
 
+	/**
+	 * @param dates Fixed time sequence provided as {@link java.util.Date} vararg
+	 */
 	public SequentialTimeSource(Date... dates) {
 		timeMillis = new long[dates.length];
 		for (int i = 0; i < timeMillis.length; i++) {
 			timeMillis[i] = dates[i].getTime();
 		}
 	}
-	
-	public SequentialTimeSource(long ... timeMillis) {
+
+	/**
+	 * @param timeMillis Fixed time sequence provided as <tt>long</tt> vararg
+	 */
+	public SequentialTimeSource(long... timeMillis) {
 		this.timeMillis = timeMillis;
 	}
 
